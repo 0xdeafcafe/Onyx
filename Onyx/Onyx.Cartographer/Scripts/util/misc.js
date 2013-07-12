2ba4e2c2a7443989d7050b2d3743bcee5bf9dd4c
Alex Reed (Xerax) <alexreed@outlook.com>
2013-07-12 19:12:04 +0100

    Themes and logos.

---
+++ Onyx/Onyx.Cartographer/Scripts/util/misc.js
@@ -0,0 +1,18 @@
+﻿$(window).ready(function () { $(window).trigger('hashchange') });
+$(window).on('hashchange', function () {
+	if (!window.location.hash)
+		return;
+
+	var windowHash = window.location.hash.substring(2).toLowerCase();
+	$('#nav-feat').removeClass('selected');
+	$('#nav-create').removeClass('selected');
+
+	switch (windowHash) {
+		case 'create':
+			$('#nav-create').addClass('selected');
+			break;
+		case 'featured':
+			$('#nav-feat').addClass('selected');
+			break;
+	}
+});
