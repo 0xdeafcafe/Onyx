d98cf55fdce72ca4adfab5908446df9b5ec84d4a
Alex Reed (Xerax) <alexreed@outlook.com>
2013-08-02 00:13:25 +0100

    Created a SignIn ViewModel

---
+++ Onyx/Onyx.Cartographer/ViewModels/User/Signin.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Onyx.Cartographer.ViewModels.User
+{
+    public class SignIn
+    {
+        /// <summary>
+        /// The User's Username
+        /// </summary>
+        [Required]
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The User's Password
+        /// </summary>
+        [Required]
+        public string Password { get; set; }
+    }
+}
