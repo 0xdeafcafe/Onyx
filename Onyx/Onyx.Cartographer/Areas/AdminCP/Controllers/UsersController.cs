2d4e511a4055b62f3dc0752cd11204a4ad73522c
Alex Reed (Xerax) <alexreed@outlook.com>
2013-08-01 18:05:14 +0100

    Added View and Edit actions to the user controller

--- Onyx/Onyx.Cartographer/Areas/AdminCP/Controllers/UsersController.cs
+++ Onyx/Onyx.Cartographer/Areas/AdminCP/Controllers/UsersController.cs
@@ -9,5 +9,20 @@ namespace Onyx.Cartographer.Areas.AdminCP.Controllers
     [RequireAdminAuthentication]
     public class UsersController : Controller
     {
+        private readonly DatabaseContext _dbContext = new DatabaseContext();
+
+        //
+        // GET: /AdminCP/Users/
+        public ActionResult Index(int? page)
+        {
+            return View(_dbContext.Users.ToPagedList(_dbContext.Users.Count(), page ?? 1, 25));
+        }
+
+        //
+        // GET: /AdminCP/Users/Edit/1
+        public ActionResult Edit(int userId)
+        {
+            return View();
+        }
 	}
 }
