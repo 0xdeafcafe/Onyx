39173abe1cc3f39f1153c21d4c9ccbe6b3de2247
Alex Reed (Xerax) <alexreed@outlook.com>
2013-08-02 23:01:19 +0100

    Get Current User helper

---
+++ Onyx/Onyx.Cartographer/Extensions/Helpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web;
+using Onyx.Cartographer.Models;
+
+namespace Onyx.Cartographer.Extensions
+{
+    public static class Helpers
+    {
+        public static User GetAuthenticatedUser()
+        {
+            var dbContext = new DatabaseContext();
+
+            // Get Session GUID
+            var sessionGuid = HttpContext.Current.Request.Cookies["SessionGuid"];
+            if (sessionGuid == null)
+                return null;
+
+            // Validate Session
+            var session = dbContext.Sessions.Find(Guid.Parse(sessionGuid.Value));
+            if (session != null && session.Expires < DateTime.UtcNow)
+                return null;
+
+            return session == null ? null : session.User;
+        }
+    }
+}
