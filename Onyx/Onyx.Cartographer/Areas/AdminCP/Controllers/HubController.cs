3c741896079f175890de966be051cc85632c7536
Alex Reed (Xerax) <alexreed@outlook.com>
2013-08-02 22:46:56 +0100

    Hub stats... begun

--- Onyx/Onyx.Cartographer/Areas/AdminCP/Controllers/HubController.cs
+++ Onyx/Onyx.Cartographer/Areas/AdminCP/Controllers/HubController.cs
@@ -1,16 +1,25 @@
+using System.Linq;
 using System.Web.Mvc;
 using Onyx.Cartographer.Extensions.Attributes;
+using Onyx.Cartographer.Models;
 
 namespace Onyx.Cartographer.Areas.AdminCP.Controllers
 {
     [RequireAdminAuthentication]
     public class HubController : Controller
     {
+        private readonly DatabaseContext _dbContext = new DatabaseContext();
+
         //
         // GET: /AdminCP/Hub/
         public ActionResult Index()
         {
-            return View();
+            ViewData["TotalUsersCount"] = _dbContext.Users.Count();
+            ViewData["TotalPrimaryUsersCount"] = _dbContext.Users.Count(u => u.Role.Identifier == Roles.PrimaryUser);
+            ViewData["TotalModeratorUsersCount"] = _dbContext.Users.Count(u => u.Role.Identifier == Roles.Moderator);
+            ViewData["TotalAdministratorUsersCount"] = _dbContext.Users.Count(u => u.Role.Identifier == Roles.Administrator);
+
+    return View();
         }
 	}
 }
