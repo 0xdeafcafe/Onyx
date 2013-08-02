f464e25561ceb1274c19ecb773267c48da39efd7
Alex Reed (Xerax) <alexreed@outlook.com>
2013-08-02 18:26:43 +0100

    Remember me on login

--- Onyx/Onyx.Cartographer/ViewModels/User/Signin.cs
+++ Onyx/Onyx.Cartographer/ViewModels/User/Signin.cs
@@ -15,5 +15,11 @@ namespace Onyx.Cartographer.ViewModels.User
         /// </summary>
         [Required]
         public string Password { get; set; }
+
+        /// <summary>
+        /// Checks if the User wants to be remembered ;_; pls
+        /// </summary>
+        [Required]
+        public bool RememberMe { get; set; }
     }
 }
