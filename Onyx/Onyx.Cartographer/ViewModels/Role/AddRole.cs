d8b48229fdf25a9c22aa4f2934c606d09ac8d0fe
Alex Reed (Xerax) <alexreed@outlook.com>
2013-08-02 12:30:16 +0100

    Added a Role ViewModel

--- Onyx/Onyx.Cartographer/ViewModels/Role/AddRole.cs
+++ Onyx/Onyx.Cartographer/ViewModels/Role/AddRole.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
+using Onyx.Cartographer.Models;
 
 namespace Onyx.Cartographer.ViewModels.Role
 {
     public class AddRole
     {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string VisualColour { get; set; }
+
+        [Required]
+        public Roles Identifier { get; set; }
     }
 }
