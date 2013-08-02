2c5152625774b38b2e6b5d9f499dc18018236d9f
Alex Reed (Xerax) <alexreed@outlook.com>
2013-08-02 12:29:10 +0100

    Add default roles on seed

--- Onyx/Onyx.Cartographer/Migrations/Configuration.cs
+++ Onyx/Onyx.Cartographer/Migrations/Configuration.cs
@@ -17,6 +17,14 @@ namespace Onyx.Cartographer.Migrations
             // Add Test Account
             if (!context.Users.Any())
                 context.Users.Add(new User { Username = "OnyxTest", Password = "fddeca1244a9e26cc1724cb98fdab6556a5d168c", Email = "xerax@xboxchaos.com", IsAdmin = true });
+
+            // Add Default Roles
+            if (!context.Roles.Any())
+            {
+                context.Roles.Add(new Role { Name = "Administrator", Description = "An Onyx Administrator", Identifier = Roles.Administrator, VisualColour = "FF0000" });
+                context.Roles.Add(new Role { Name = "Moderator", Description = "An Onyx Moderator", Identifier = Roles.Moderator, VisualColour = "0033CC" });
+                context.Roles.Add(new Role { Name = "User", Description = "A Regular User", Identifier = Roles.PrimaryUser, VisualColour = "C0C0C0" });
+            }
         }
     }
 }
