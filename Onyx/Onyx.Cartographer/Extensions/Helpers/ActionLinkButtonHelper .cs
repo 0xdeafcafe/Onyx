c855322751a2677418397fe506db6681b296887e
Alex Reed (Xerax) <alexreed@outlook.com>
2013-08-02 10:50:30 +0100

    Added a HTML Helper for Buttons

---
+++ Onyx/Onyx.Cartographer/Extensions/Helpers/ActionLinkButtonHelper .cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Routing;
+
+// ReSharper disable once CheckNamespace
+namespace System.Web.Mvc.Html
+{
+
+    public static class ActionLinkButtonHelper
+    {
+        public static MvcHtmlString ActionLinkButton(this HtmlHelper htmlHelper, string buttonText, string actionName, string controllerName)
+        {
+            var href = UrlHelper.GenerateUrl("default", actionName, controllerName, new RouteValueDictionary(), RouteTable.Routes, htmlHelper.ViewContext.RequestContext, false);
+            var buttonHtml = string.Format("<input type=\"button\" title=\"{0}\" value=\"{0}\" onclick=\"location.href='{1}'\" class=\"button\" />", buttonText, href);
+            return new MvcHtmlString(buttonHtml);
+        }
+
+        public static MvcHtmlString ActionLinkButton(this HtmlHelper htmlHelper, string buttonText, string actionName, string controllerName, RouteValueDictionary routeValues)
+        {
+            var href = UrlHelper.GenerateUrl("default", actionName, controllerName, routeValues, RouteTable.Routes, htmlHelper.ViewContext.RequestContext, false);
+            var buttonHtml = string.Format("<input type=\"button\" title=\"{0}\" value=\"{0}\" onclick=\"location.href='{1}'\" class=\"button\" />", buttonText, href);
+            return new MvcHtmlString(buttonHtml);
+        }
+    }
+}
