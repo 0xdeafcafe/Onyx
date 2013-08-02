ebdb6a30a8035762193037987d09735bff8a4d3e
Alex Reed (Xerax) <alexreed@outlook.com>
2013-08-02 20:41:10 +0100

    Added a VariousFunctions class

---
+++ Onyx/Onyx.Cartographer/Extensions/VariousFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Onyx.Cartographer.Extensions
+{
+    public class VariousFunctions
+    {
+        public static string ByteArrayToHexString(byte[] bytes)
+        {
+            return BitConverter.ToString(bytes).Replace("-", string.Empty);
+        }
+
+        public static byte[] HexStringToByteArray(string hexString)
+        {
+            return Enumerable.Range(0, hexString.Length)
+                     .Where(x => x % 2 == 0)
+                     .Select(x => Convert.ToByte(hexString.Substring(x, 2), 16))
+                     .ToArray();
+        }
+    }
+}
