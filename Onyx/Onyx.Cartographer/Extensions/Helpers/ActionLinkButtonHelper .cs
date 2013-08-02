7afd5a5630169db9680c14d0795934aedd3984a6
Alex Reed (Xerax) <alexreed@outlook.com>
2013-08-02 18:26:53 +0100

    Updated Action Link Button Helper

--- Onyx/Onyx.Cartographer/Extensions/Helpers/ActionLinkButtonHelper .cs
+++ Onyx/Onyx.Cartographer/Extensions/Helpers/ActionLinkButtonHelper .cs
@@ -6,16 +6,9 @@ namespace System.Web.Mvc.Html
 
     public static class ActionLinkButtonHelper
     {
-        public static MvcHtmlString ActionLinkButton(this HtmlHelper htmlHelper, string buttonText, string actionName, string controllerName)
+        public static MvcHtmlString ActionLinkButton(this HtmlHelper htmlHelper, string buttonText, string actionName, string controllerName, string routeName = "default")
         {
-            var href = UrlHelper.GenerateUrl("default", actionName, controllerName, new RouteValueDictionary(), RouteTable.Routes, htmlHelper.ViewContext.RequestContext, false);
-            var buttonHtml = string.Format("<input type=\"button\" title=\"{0}\" value=\"{0}\" onclick=\"location.href='{1}'\" class=\"button\" />", buttonText, href);
-            return new MvcHtmlString(buttonHtml);
-        }
-
-        public static MvcHtmlString ActionLinkButton(this HtmlHelper htmlHelper, string buttonText, string actionName, string controllerName, RouteValueDictionary routeValues)
-        {
-            var href = UrlHelper.GenerateUrl("default", actionName, controllerName, routeValues, RouteTable.Routes, htmlHelper.ViewContext.RequestContext, false);
+            var href = UrlHelper.GenerateUrl(routeName, actionName, controllerName, new RouteValueDictionary(), RouteTable.Routes, htmlHelper.ViewContext.RequestContext, false);
             var buttonHtml = string.Format("<input type=\"button\" title=\"{0}\" value=\"{0}\" onclick=\"location.href='{1}'\" class=\"button\" />", buttonText, href);
             return new MvcHtmlString(buttonHtml);
         }
