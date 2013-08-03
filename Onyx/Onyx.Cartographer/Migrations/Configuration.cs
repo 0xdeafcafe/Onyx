7c3bfef44762d1f47f382d718e9a36ac3a28afe9
Alex Reed (Xerax) <alexreed@outlook.com>
2013-08-03 17:16:31 +0100

    save le changes after each entry add.

--- Onyx/Onyx.Cartographer/Migrations/Configuration.cs
+++ Onyx/Onyx.Cartographer/Migrations/Configuration.cs
@@ -20,11 +20,21 @@ namespace Onyx.Cartographer.Migrations
                 context.Roles.Add(new Role { Name = "Administrator", Description = "An Onyx Administrator", Identifier = Roles.Administrator, VisualColour = "FF0000" });
                 context.Roles.Add(new Role { Name = "Moderator", Description = "An Onyx Moderator", Identifier = Roles.Moderator, VisualColour = "0033CC" });
                 context.Roles.Add(new Role { Name = "User", Description = "A Regular User", Identifier = Roles.PrimaryUser, VisualColour = "C0C0C0" });
+                context.SaveChanges();
             }
 
             // Add Test Account
             if (!context.Users.Any())
-                context.Users.Add(new User { Username = "OnyxTest", Password = "1000:c1JY6wtgSaAoIL2b8TDp7MCJeMyDO4A/:fkGpJleo1noIkvhC6gzHM4vNZjPMr0mz", Email = "xerax@xboxchaos.com", RoleId = context.Roles.First(r => r.Identifier == Roles.Administrator).Id });
+            {
+                context.Users.Add(new User
+                {
+                    Username = "OnyxTest",
+                    Password = "1000:c1JY6wtgSaAoIL2b8TDp7MCJeMyDO4A/:fkGpJleo1noIkvhC6gzHM4vNZjPMr0mz",
+                    Email = "xerax@xboxchaos.com",
+                    RoleId = context.Roles.First(r => r.Identifier == Roles.Administrator).Id
+                });
+                context.SaveChanges();
+            }
         }
     }
 }
